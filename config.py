c0fc6e369a0c2a98100ccc1d086b6e66eaefb57b
Emile Jansons <emilejansons@gmail.com>
2017-07-04 16:47:44 +1000

    first commit

---
+++ config.py
@@ -0,0 +1,37 @@
+# config.py
+
+class Config(object):
+    """
+    Common configurations
+    """    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+
+    TESTING = True
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
